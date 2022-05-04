eda63582229078b0b6ad6e5bfbf295c0903b17f6
void <92902636+thecatvoid@users.noreply.github.com>
2022-05-04 19:54:28 +0530

    More kernel cmdline parameters

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -40,6 +40,7 @@ androidboot.memcg=1 lpm_levels.sleep_disabled=1 \
 video=vfb:640x400,bpp=32,memsize=3072000 \
 msm_rtb.filter=0x237 service_locator.enable=1 \
 swiotlb=2048 loop.max_part=7 androidboot.hab.csv=16 \
+firmware_class.path=/vendor/firmware_mnt/image \
 androidboot.hab.product=guamp androidboot.hab.cid=50 buildvariant=eng	
 
 BOARD_KERNEL_CMDLINE += androidboot.selinux=permissive
